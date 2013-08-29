1cf76f4f4e8320f873a1c75b956912713b65a241
Dave Woodall <dave@hireDave.me>
2013-08-29 11:54:46 -0600

    Basic Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery
+  http_basic_authenticate_with name: ENV['USER'], password: ENV['PASSWORD']
 end
