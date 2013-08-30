098f82b3fa482376db126cd38ff6cad515e6a32a
Aaron Gray <aaron@hellometa.com>
2013-08-29 18:06:53 -0600

    Includes JS to target tasks with AT symbol

--- db/schema.rb
+++ db/schema.rb
@@ -13,147 +13,12 @@
 
 ActiveRecord::Schema.define(:version => 20130829214242) do
 
-  create_table "cities", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.string   "name"
-    t.string   "slug"
-  end
-
-  add_index "cities", ["slug"], :name => "index_cities_on_slug"
-
   create_table "comments", :force => true do |t|
     t.text     "entry"
     t.string   "tags"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "task_id"
-  end
-
-  create_table "confirmations", :force => true do |t|
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.string   "color"
-    t.string   "garment"
-    t.integer  "printer_id"
-    t.string   "city"
-    t.integer  "total_invoice"
-    t.integer  "piece_price"
-    t.string   "name"
-    t.string   "email"
-  end
-
-  create_table "finishing_services", :force => true do |t|
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.boolean  "printed_labels"
-    t.boolean  "woven_labels"
-    t.boolean  "folding_bagging"
-    t.boolean  "hang_tags"
-    t.boolean  "fulfillment"
-    t.integer  "printer_id"
-  end
-
-  add_index "finishing_services", ["printer_id"], :name => "index_finishing_services_on_printer_id"
-
-  create_table "garment_prices", :force => true do |t|
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.string   "fabric"
-    t.string   "cut"
-    t.string   "quality"
-    t.string   "colors"
-    t.string   "inventory"
-    t.string   "brand"
-    t.string   "style_number"
-    t.integer  "price"
-    t.integer  "printer_id"
-  end
-
-  add_index "garment_prices", ["printer_id"], :name => "index_garment_prices_on_printer_id"
-
-  create_table "garment_selectors", :force => true do |t|
-    t.string   "short_sleeve_tshirt"
-    t.integer  "printer_id"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
-    t.text     "comment"
-    t.string   "garment_price"
-  end
-
-  add_index "garment_selectors", ["printer_id"], :name => "index_garment_selectors_on_printer_id"
-
-  create_table "print_prices", :force => true do |t|
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-    t.string   "pallet_type"
-    t.string   "ink_type"
-    t.integer  "base_color"
-    t.integer  "one_color"
-    t.integer  "two_color"
-    t.integer  "three_color"
-    t.integer  "four_color"
-    t.integer  "five_color"
-    t.integer  "six_color"
-    t.integer  "seven_color"
-    t.integer  "eight_color"
-    t.integer  "printer_id"
-    t.integer  "price_tier",  :limit => 255
-  end
-
-  add_index "print_prices", ["printer_id"], :name => "index_print_prices_on_printer_id"
-
-  create_table "printer_features", :force => true do |t|
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.boolean  "free_shipping"
-    t.boolean  "digital_proof"
-    t.boolean  "hotline"
-    t.boolean  "art_team"
-    t.boolean  "sample"
-    t.boolean  "drop_ship"
-    t.integer  "printer_id"
-  end
-
-  add_index "printer_features", ["printer_id"], :name => "index_printer_features_on_printer_id"
-
-  create_table "printers", :force => true do |t|
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-    t.string   "email"
-    t.string   "contact_name"
-    t.string   "phone"
-    t.string   "address"
-    t.integer  "zipcode"
-    t.string   "website"
-    t.boolean  "pending"
-    t.string   "shop_name"
-    t.integer  "city_id"
-    t.string   "slug"
-    t.text     "about"
-    t.text     "overview"
-    t.text     "services"
-    t.text     "customer_service"
-    t.text     "terms"
-  end
-
-  add_index "printers", ["slug"], :name => "index_printers_on_slug"
-
-  create_table "quotes", :force => true do |t|
-    t.integer  "quantity"
-    t.string   "number_of_colors"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-  end
-
-  create_table "sessions", :force => true do |t|
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.string   "session_id"
-    t.integer  "quote_id"
-    t.integer  "garment_id"
-    t.integer  "printer_id"
-    t.integer  "garment_price"
   end
 
   create_table "task_counters", :force => true do |t|
@@ -171,17 +36,5 @@ ActiveRecord::Schema.define(:version => 20130829214242) do
     t.string   "tags"
     t.boolean  "deleted"
   end
-
-  create_table "users", :force => true do |t|
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
-    t.string   "email",                             :null => false
-    t.string   "encrypted_password", :limit => 128, :null => false
-    t.string   "confirmation_token", :limit => 128
-    t.string   "remember_token",     :limit => 128, :null => false
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email"
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
