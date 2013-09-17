cf9bf065078911710cec5d7f305b3d5b89f0c7ca
Dave Woodall <dave@hireDave.me>
2013-09-17 08:32:05 -0600

    add some questions.

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,12 +1,6 @@
-// How do I add a class 'highlight' to any row who's tag includes an @
-
-$(document).ready(function(){
-  $( "td:contains('@')" ).parent().addClass("highlight");
-  $( "td:contains('zzz')" ).parent().addClass("snooze");
-
-  var fixHelperModified = function(e, tr) {
+  var fixHelperModified = function(e, tr) {    // what is the e for?
     var $originals = tr.children();
-    var $helper = tr.clone();
+    var $helper = tr.clone();                  // what's a way to pry into code like this to understand what's happening?
     $helper.children().each(function(index)
     {
       $(this).width($originals.eq(index).width())
@@ -14,12 +8,17 @@ $(document).ready(function(){
     return $helper;
   };
 
+$(document).ready(function(){
+  $( "td:contains('@')" ).parent().addClass("highlight");
+  $( "td:contains('zzz')" ).parent().addClass("snooze");
+
   $("#sortable tbody").sortable({
     helper: fixHelperModified,
     axis: 'y',
     update: function(){
-      $.post($(this).data('update-url'), $(this).sortable('serialize'))
-    }
-  }).disableSelection();
+      $.post($(this).data('update-url'), $(this).sortable('serialize')) // I have no idea what this code is doing.
+    }                                                                   // Goal is to fire task#sort but doesn't seem to
+                                                                        // be calling that method
+  }).disableSelection();  // What does this do? I didn't find it on Jquery ui
 });
 
