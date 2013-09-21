e0a27acb7d69120e64bf1b867f65a281dfd83a0d
Dave Woodall <dave@hireDave.me>
2013-09-21 09:33:18 -0600

    Remove old code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-private
-
-  def current_user # Doing railscast, but I think this overrides clearance.
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  helper_method :current_user
 end
