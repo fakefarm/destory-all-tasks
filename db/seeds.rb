fc5ea516ef37dac9faf9cc30aad83801e7cedc4a
Dave Woodall <dave@hireDave.me>
2013-11-06 12:47:25 -0700

    resolve precompile error

--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-Task.destroy_all
-Task.create(item: 'Take out the trash', tags: 'chore', user_id: 1)
-Task.create(item: 'Clean my room', tags: 'chore', user_id: 1)
-Task.create(item: 'Make my bed', tags: 'chore', user_id: 1)
-Task.create(item: 'Learn to code', tags: 'school', user_id: 1)
-Task.create(item: 'Read about Angular', tags: 'school', user_id: 1)
-Task.create(item: 'Write better tests', tags: 'school', user_id: 1)
