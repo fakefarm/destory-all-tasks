e026a9b0cf0ecd8016a9053d284d850e20c9a811
Dave Woodall <dave@hireDave.me>
2013-11-11 10:10:34 -0700

    Heroku Redis Configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 DestroyAllTasks::Application.configure do
 
 
+  # Redis Action
+  ENV["REDISTOGO_URL"] = REDISTOGO_URL
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
