d8b31f8ee0d3be09586e9cb2e3977775ec7ab8a6
Dave Woodall <dave@hireDave.me>
2013-11-16 12:06:37 -0700

    Create Profile Observer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-
-
   def index
     @users = User.all
   end
@@ -12,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+        CreateProfileObserver.call(@user)
         cookies[:auth_token] = @user.auth_token
         redirect_to tasks_path, notice: "Destroy some!"
     else
