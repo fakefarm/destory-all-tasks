e927a48e735274f6c9c6c9e4840496a41d2d876a
Dave Woodall <dave@hireDave.me>
2013-11-16 12:06:07 -0700

    Profile Resource

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,22 @@
+class ProfilesController < ApplicationController
+  def show
+    @profile = Profile.find(current_user.profile.id)
+  end
+
+  def edit
+    # require 'pry'; binding.pry
+    @profile = Profile.find(current_user.profile_id)
+    @users = @profile.users
+  end
+
+  def update
+    @profile = Profile.find(current_user.profile_id)
+    @profile.update_attributes(name: params[:profile][:name])
+    User.find(current_user.id).update_attributes(params[:profile][:user])
+    if @profile.save
+      redirect_to profile_path(@profile)
+    else
+      render :edit
+    end
+  end
+end
