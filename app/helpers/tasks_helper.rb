7aba7b8b81e4fcf21dcfcd0b9b3daf58e847ed67
Dave Woodall <dave@hireDave.me>
2013-11-30 12:24:21 -0700

    Clean up

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -16,9 +16,9 @@ module TasksHelper
 
   def flex_destroy_path(task)
     if task.comments.count > 0
-     link_to "#{pluralize(task.comments.count, 'note')}", task_path(task)
+      link_to "#{pluralize(task.comments.count, 'note')}", task_path(task)
     else
-     link_to 'Destroy', task, :method => :delete
-   end
- end
+      link_to 'Destroy', task, method: :delete
+    end
+  end
 end
